41be5f546a30481090f504802329c1ccdbe73bd8
Partha Pratim Nandi <imppratim@gmail.com>
2025-10-18 22:29:23 +0200

    changes in the Heroscection

--- portfolio_webapp/src/data/meinDaten.ts
+++ portfolio_webapp/src/data/meinDaten.ts
@@ -4,6 +4,6 @@ const myPeronalData = {
   title: "Artificial Intelligence Engineer",
   image: img,
   intro:
-    "Artificial Intelligence Engineer based in Erlangen, Germany, with expertise in machine learning and web development. I am proficient in developing AI solutions and creating interactive, accessible web interfaces that enhance user experience.Skilled in Python, React, JavaScript, TypeScript, and RESTful APIs, my experience includes developing UI components for web apps, as well as integrating ML workflows.",
+    "I am an Artificial Intelligence Engineer based in Erlangen, Germany, specializing in machine learning and web development. I have a strong passion for exploring computer vision models and creating interactive, accessible web interfaces that deliver exceptional user experiences. Beyond work, I enjoy hiking, watching football, cricket, and occasionally playing video games. Recently, I have embraced playing the piano as a new hobby. In the summer, I actively play cricket a few times a month. Although fishing remains one of my favorite pastimes, relocating to Germany has limited the time I can dedicate to it.",
 };
 export default myPeronalData;
